b80629ba43a1de4c8374b031a6f2dd4d7f920c0f
eric-kitagawa <kitagawa.e@northeastern.edu>
2024-11-22 22:45:54 -0500

    Added lab5

---
+++ Lab5/WorkingWithArrays.js
@@ -0,0 +1,17 @@
+let todos = [{ id: 1, title: "Task 1", completed: false }, { id: 2, title: "Task 2", completed: true },
+{ id: 3, title: "Task 3", completed: false }, { id: 4, title: "Task 4", completed: true },];
+export default function WorkingWithArrays(app) {
+    app.get("/lab5/todos", (req, res) => {
+        const { completed } = req.query;
+        const { id } = req.params;
+        const todo = todos.find((t) => t.id === parseInt(id));
+        if (completed !== undefined) {
+            const completedBool = completed === "true";
+            const completedTodos = todos.filter(
+                (t) => t.completed === completedBool);
+            res.json(completedTodos);
+            return;
+        }
+        res.json(todos);
+    });
+};
