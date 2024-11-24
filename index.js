78c0918d64301ff0077aff1dabb54fb9b905852a
eric-kitagawa <kitagawa.e@northeastern.edu>
2024-11-23 21:34:49 -0500

    Added lab 5

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
+import session from "express-session";
 import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
+import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+
 const app = express();
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:3000",
+    })
+);
+
 app.use(express.json());
+
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kanbas",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
+UserRoutes(app);
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000)
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT || 4000}`);
+});
